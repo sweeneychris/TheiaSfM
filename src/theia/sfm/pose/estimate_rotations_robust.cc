9b71f9aab7e76e09c41f0176444df96447e2edaa
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2015-02-18 23:27:28 +0100

    Robust global rotation solver
    Change-Id: If743c41b5692a400150daa51cfa3407bcf72453b

---
+++ src/theia/sfm/pose/estimate_rotations_robust.cc
@@ -0,0 +1,269 @@
+// Copyright (C) 2015 The Regents of the University of California (Regents).
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of The Regents or University of California nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Chris Sweeney (cmsweeney@cs.ucsb.edu)
+
+#include "theia/sfm/pose/estimate_rotations_robust.h"
+
+#include <ceres/rotation.h>
+#include <Eigen/Core>
+#include <Eigen/SparseCore>
+#include <Eigen/SparseCholesky>
+#include <unordered_map>
+
+#include "theia/math/l1_solver.h"
+#include "theia/sfm/types.h"
+#include "theia/util/hash.h"
+#include "theia/util/map_util.h"
+
+
+namespace theia {
+
+namespace {
+
+// Computes the relative rotation error from the global rotations to the
+// relative rotation. The error is returned in angle axis form.
+Eigen::Vector3d ComputeRelativeRotationError(
+    const Eigen::Vector3d& relative_rotation, const Eigen::Vector3d& rotation1,
+    const Eigen::Vector3d& rotation2) {
+  Eigen::Matrix3d relative_rotation_matrix, rotation_matrix1, rotation_matrix2;
+  ceres::AngleAxisToRotationMatrix(
+      relative_rotation.data(),
+      ceres::ColumnMajorAdapter3x3(relative_rotation_matrix.data()));
+  ceres::AngleAxisToRotationMatrix(
+      rotation1.data(), ceres::ColumnMajorAdapter3x3(rotation_matrix1.data()));
+  ceres::AngleAxisToRotationMatrix(
+      rotation2.data(), ceres::ColumnMajorAdapter3x3(rotation_matrix2.data()));
+
+  // Compute the relative rotation error.
+  const Eigen::Matrix3d relative_rotation_matrix_error =
+      rotation_matrix2.transpose() * relative_rotation_matrix *
+      rotation_matrix1;
+  Eigen::Vector3d relative_rotation_error;
+  ceres::RotationMatrixToAngleAxis(
+      ceres::ColumnMajorAdapter3x3(relative_rotation_matrix_error.data()),
+      relative_rotation_error.data());
+  return relative_rotation_error;
+}
+
+// Applies the rotation change to the rotation.
+void ApplyRotation(const Eigen::Vector3d& rotation_change,
+                   Eigen::Vector3d* rotation) {
+  // Convert to rotation matrices.
+  Eigen::Matrix3d rotation_change_matrix, rotation_matrix;
+  ceres::AngleAxisToRotationMatrix(
+      rotation_change.data(),
+      ceres::ColumnMajorAdapter3x3(rotation_change_matrix.data()));
+  ceres::AngleAxisToRotationMatrix(
+      rotation->data(), ceres::ColumnMajorAdapter3x3(rotation_matrix.data()));
+
+  // Apply the rotation change.
+  const Eigen::Matrix3d changed_rotation =
+      rotation_matrix * rotation_change_matrix;
+  // Convert back to angle axis.
+  ceres::RotationMatrixToAngleAxis(
+      ceres::ColumnMajorAdapter3x3(changed_rotation.data()), rotation->data());
+}
+
+}  // namespace
+
+bool RobustRotationEstimator::EstimateRotations(
+    std::unordered_map<ViewId, Eigen::Vector3d>* global_orientations) {
+  global_orientations_ = global_orientations;
+
+  // Compute a mapping of view ids to indices in the linear system. One matrix
+  // will have an index of -1 and will not be added to the linear system. This
+  // will remove the gauge freedom (effectively holding one camera as the
+  // identity rotation).
+  int index = -1;
+  view_id_to_index_.reserve(global_orientations->size());
+  for (const auto& orientation : *global_orientations) {
+    view_id_to_index_[orientation.first] = index;
+    ++index;
+  }
+
+  Eigen::SparseMatrix<double> sparse_mat;
+  SetupLinearSystem();
+
+  if (!SolveL1Regression()) {
+    LOG(ERROR) << "Could not solve the L1 regression step.";
+    return false;
+  }
+
+  if (!SolveIRLS()) {
+    LOG(ERROR) << "Could not solve the least squares error step.";
+  }
+
+  return true;
+}
+
+// Set up the sparse linear system.
+void RobustRotationEstimator::SetupLinearSystem() {
+  // The rotation change is one less than the number of global rotations because
+  // we keep one rotation constant.
+  rotation_change_.resize((global_orientations_->size() - 1) * 3);
+  relative_rotation_error_.resize(relative_rotations_.size() * 3);
+  sparse_matrix_.resize(relative_rotations_.size() * 3,
+                        (global_orientations_->size() - 1) * 3);
+
+  // For each relative rotation constraint, add an entry to the sparse
+  // matrix. We use the first order approximation of angle axis such that:
+  // R_ij = R_j - R_i. This makes the sparse matrix just a bunch of identity
+  // matrices.
+  int rotation_error_index = 0;
+  for (const auto& relative_rotation : relative_rotations_) {
+    const int view1_index =
+        FindOrDie(view_id_to_index_, relative_rotation.first.first);
+    if (view1_index != kConstantRotationIndex) {
+      sparse_matrix_.insert(3 * rotation_error_index + 0, 3 * view1_index + 0) =
+          -1.0;
+      sparse_matrix_.insert(3 * rotation_error_index + 1, 3 * view1_index + 1) =
+          -1.0;
+      sparse_matrix_.insert(3 * rotation_error_index + 2, 3 * view1_index + 2) =
+          -1.0;
+    }
+
+    const int view2_index =
+        FindOrDie(view_id_to_index_, relative_rotation.first.second);
+    if (view2_index != kConstantRotationIndex) {
+      sparse_matrix_.insert(3 * rotation_error_index + 0, 3 * view2_index + 0) =
+          1.0;
+      sparse_matrix_.insert(3 * rotation_error_index + 1, 3 * view2_index + 1) =
+          1.0;
+      sparse_matrix_.insert(3 * rotation_error_index + 2, 3 * view2_index + 2) =
+          1.0;
+    }
+
+    ++rotation_error_index;
+  }
+}
+
+// Computes the relative rotation error based on the current global
+// orientation estimates.
+void RobustRotationEstimator::ComputeRotationError() {
+  int rotation_error_index = 0;
+  for (const auto& relative_rotation : relative_rotations_) {
+    relative_rotation_error_.segment<3>(3 * rotation_error_index) =
+        ComputeRelativeRotationError(
+            relative_rotation.second,
+            FindOrDie(*global_orientations_, relative_rotation.first.first),
+            FindOrDie(*global_orientations_, relative_rotation.first.second));
+    ++rotation_error_index;
+  }
+}
+
+bool RobustRotationEstimator::SolveL1Regression() {
+  static const double kConvergenceThreshold = 1e-3;
+
+  L1Solver<Eigen::SparseMatrix<double> >::Options options;
+  L1Solver<Eigen::SparseMatrix<double> > l1_solver(options, sparse_matrix_);
+
+  rotation_change_.setZero();
+  for (int i = 0; i < options_.max_num_l1_iterations; i++) {
+    ComputeRotationError();
+    l1_solver.Solve(relative_rotation_error_, &rotation_change_);
+    UpdateGlobalRotations();
+
+    if (relative_rotation_error_.norm() < kConvergenceThreshold) {
+      break;
+    }
+  }
+  return true;
+}
+
+// Update the global orientations using the current value in the
+// rotation_change.
+void RobustRotationEstimator::UpdateGlobalRotations() {
+  for (auto& rotation : *global_orientations_) {
+    const int view_index = FindOrDie(view_id_to_index_, rotation.first);
+    if (view_index == kConstantRotationIndex) {
+      continue;
+    }
+
+    // Apply the rotation change to the global orientation.
+    const Eigen::Vector3d& rotation_change =
+        rotation_change_.segment<3>(3 * view_index);
+    ApplyRotation(rotation_change, &rotation.second);
+  }
+}
+
+bool RobustRotationEstimator::SolveIRLS() {
+  static const double kConvergenceThreshold = 1e-3;
+  static const double kDeltaSq = 1e-8;
+
+  // Set up the linear solver and analyze the sparsity pattern of the
+  // system. Since the sparsity pattern will not change with each linear solve
+  // this can help speed up the solution time.
+  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double> > linear_solver;
+  linear_solver.analyzePattern(sparse_matrix_.transpose() * sparse_matrix_);
+  if (linear_solver.info() != Eigen::Success) {
+    LOG(ERROR) << "Cholesky decomposition failed.";
+    return false;
+  }
+
+  Eigen::ArrayXd errors, weights;
+  for (int i = 0; i < options_.max_num_irls_iterations; i++) {
+    const Eigen::VectorXd prev_rotation_change = rotation_change_;
+    ComputeRotationError();
+
+    // Compute the weights for each error term.
+    errors =
+        (sparse_matrix_ * rotation_change_ - relative_rotation_error_).array();
+    weights = kDeltaSq / (errors.square() + kDeltaSq).square();
+
+    // Update the factorization for the weighted values.
+    const Eigen::SparseMatrix<double> at_weight =
+        sparse_matrix_.transpose() * weights.matrix().asDiagonal();
+    linear_solver.factorize(at_weight * sparse_matrix_);
+    if (linear_solver.info() != Eigen::Success) {
+      LOG(ERROR) << "Failed to solve the least squares system.";
+      return false;
+    }
+
+    // Solve the least squares problem..
+    rotation_change_ =
+        linear_solver.solve(at_weight * relative_rotation_error_);
+    if (linear_solver.info() != Eigen::Success) {
+      LOG(ERROR) << "Failed to solve the least squares system.";
+      return false;
+    }
+
+    UpdateGlobalRotations();
+    if ((prev_rotation_change - rotation_change_).squaredNorm() <
+        kConvergenceThreshold) {
+      break;
+    }
+  }
+  return true;
+}
+
+}  // namespace theia
---
+++ src/theia/sfm/transformation/align_rotations.cc
@@ -0,0 +1,158 @@
+// Copyright (C) 2015 The Regents of the University of California (Regents).
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of The Regents or University of California nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Chris Sweeney (cmsweeney@cs.ucsb.edu)
+
+#include "theia/sfm/transformation/align_rotations.h"
+
+#include <ceres/ceres.h>
+#include <ceres/rotation.h>
+#include <Eigen/Core>
+#include <glog/logging.h>
+#include <vector>
+
+namespace theia {
+
+namespace {
+
+// A cost function whose error is the difference in rotations after the current
+// alignemnt is applied. That is,
+//    error = unaligned_rotation * rotation_alignment - gt_rotation.
+struct RotationAlignmentError {
+  RotationAlignmentError(const Eigen::Vector3d& gt_rotation,
+                         const Eigen::Vector3d& unaligned_rotation)
+      : gt_rotation_(gt_rotation) {
+    // Convert the unaligned rotation to rotation matrix.
+    Eigen::Matrix3d unaligned_rotation_mat;
+    ceres::AngleAxisToRotationMatrix(
+        unaligned_rotation.data(),
+        ceres::ColumnMajorAdapter3x3(unaligned_rotation_mat_.data()));
+  }
+
+  // Compute the alignment error of the two rotations after applying the
+  // rotation transformation.
+  template <typename T>
+  bool operator()(const T* rotation, T* residuals) const {
+    // Convert the rotation transformation to a matrix.
+    Eigen::Matrix<T, 3, 3> rotation_mat;
+    ceres::AngleAxisToRotationMatrix(
+        rotation, ceres::ColumnMajorAdapter3x3(rotation_mat.data()));
+
+    // Apply the rotation transformation.
+    const Eigen::Matrix<T, 3, 3> aligned_rotation_mat =
+        unaligned_rotation_mat_.cast<T>() * rotation_mat;
+
+    // Convert back to angle axis.
+    Eigen::Matrix<T, 3, 1> aligned_rotation;
+    ceres::RotationMatrixToAngleAxis(
+        ceres::ColumnMajorAdapter3x3(aligned_rotation_mat.data()),
+        aligned_rotation.data());
+
+    // Compute the error of the aligned rotation to the gt_rotation.
+    residuals[0] = T(gt_rotation_[0]) - aligned_rotation[0];
+    residuals[1] = T(gt_rotation_[1]) - aligned_rotation[1];
+    residuals[2] = T(gt_rotation_[2]) - aligned_rotation[2];
+
+    return true;
+  }
+
+  static ceres::CostFunction* Create(
+      const Eigen::Vector3d& gt_rotation,
+      const Eigen::Vector3d& unaligned_rotation) {
+    return new ceres::AutoDiffCostFunction<RotationAlignmentError, 3, 3>(
+        new RotationAlignmentError(gt_rotation, unaligned_rotation));
+  }
+
+  Eigen::Vector3d gt_rotation_;
+  Eigen::Matrix3d unaligned_rotation_mat_;
+};
+
+// Apply the rotation alignment to all rotations in the vector.
+void ApplyRotationTransformation(const Eigen::Vector3d& rotation_alignment,
+                                 std::vector<Eigen::Vector3d>* rotation) {
+  Eigen::Matrix3d rotation_alignment_mat;
+  ceres::AngleAxisToRotationMatrix(
+      rotation_alignment.data(),
+      ceres::ColumnMajorAdapter3x3(rotation_alignment_mat.data()));
+
+  for (int i = 0; i < rotation->size(); i++) {
+    // Convert the current rotation to a rotation matrix.
+    Eigen::Matrix3d rotation_mat;
+    ceres::AngleAxisToRotationMatrix(
+        rotation->at(i).data(),
+        ceres::ColumnMajorAdapter3x3(rotation_mat.data()));
+
+    // Apply the rotation transformation.
+    const Eigen::Matrix3d aligned_rotation =
+        rotation_mat * rotation_alignment_mat;
+
+    // Convert back to angle axis.
+    ceres::RotationMatrixToAngleAxis(
+        ceres::ColumnMajorAdapter3x3(aligned_rotation.data()),
+        rotation->at(i).data());
+  }
+}
+
+}  // namespace
+
+// We solve a nonlinear least squares system to determine a rotation R that will
+// align the rotation to the gt_rotation such that rotation * R = gt_rotation.
+// This could potentially be set up as a linear system, however, that does not
+// guarantee that R will remaind a valid rotation. Instead, we simply use a
+// nonlinear system to ensure that R is a valid rotation.
+void AlignRotations(const std::vector<Eigen::Vector3d>& gt_rotation,
+                    std::vector<Eigen::Vector3d>* rotation) {
+  CHECK_EQ(gt_rotation.size(), rotation->size());
+
+  Eigen::Vector3d rotation_alignment = Eigen::Vector3d::Zero();
+
+  // Set up the nonlinear system and adds all residuals.
+  ceres::Problem problem;
+  for (int i = 0; i < gt_rotation.size(); i++) {
+    problem.AddResidualBlock(RotationAlignmentError::Create(gt_rotation[i],
+                                                            rotation->at(i)),
+                             NULL,
+                             rotation_alignment.data());
+  }
+
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_QR;
+  options.function_tolerance = 0.0;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+  VLOG(2) << summary.FullReport();
+
+  // Apply the solved rotation transformation to the rotations.
+  ApplyRotationTransformation(rotation_alignment, rotation);
+}
+
+}  // namespace theia
