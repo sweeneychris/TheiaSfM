e93c7f94c4cdf8f34a62900a38e40ae72b5f6f83
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2015-03-21 15:16:27 +0100

    Robust reconstruction alignment
    Change-Id: I38228cbd3e14a0d988b61a78173630ab63d36c06

--- src/theia/sfm/transformation/align_reconstructions.cc
+++ src/theia/sfm/transformation/align_reconstructions.cc
@@ -44,8 +44,60 @@
 #include "theia/sfm/transformation/align_point_clouds.h"
 #include "theia/sfm/transformation/transform_reconstruction.h"
 #include "theia/sfm/types.h"
+#include "theia/solvers/ransac.h"
+#include "theia/solvers/sample_consensus_estimator.h"
 
 namespace theia {
+namespace {
+
+struct CameraCorrespondence {
+  Eigen::Vector3d camera1;
+  Eigen::Vector3d camera2;
+};
+
+struct SimilarityTransformation {
+  Eigen::Matrix3d rotation;
+  Eigen::Vector3d translation;
+  double scale;
+};
+
+class CameraAlignmentEstimator
+    : public Estimator<CameraCorrespondence, SimilarityTransformation> {
+ public:
+  CameraAlignmentEstimator() {}
+
+  double SampleSize() const { return 4; }
+
+  bool EstimateModel(
+      const std::vector<CameraCorrespondence>& correspondences,
+      std::vector<SimilarityTransformation>* sim_transforms) const {
+    SimilarityTransformation sim_transform;
+    std::vector<Eigen::Vector3d> positions1(correspondences.size());
+    std::vector<Eigen::Vector3d> positions2(correspondences.size());
+    for (int i = 0; i < correspondences.size(); i++) {
+      positions1[i] = correspondences[i].camera1;
+      positions2[i] = correspondences[i].camera2;
+    }
+
+    AlignPointCloudsUmeyama(positions2,
+                            positions1,
+                            &sim_transform.rotation,
+                            &sim_transform.translation,
+                            &sim_transform.scale);
+    sim_transforms->emplace_back(sim_transform);
+    return true;
+  }
+
+  double Error(const CameraCorrespondence& cameras,
+               const SimilarityTransformation& sim_transform) const {
+    const Eigen::Vector3d transformed_camera =
+        sim_transform.scale * sim_transform.rotation * cameras.camera2 +
+        sim_transform.translation;
+    return (cameras.camera1 - transformed_camera).squaredNorm();
+  }
+};
+
+}  // namespace
 
 void AlignReconstructions(const Reconstruction& reconstruction1,
                           Reconstruction* reconstruction2) {
@@ -66,6 +118,69 @@ void AlignReconstructions(const Reconstruction& reconstruction1,
         reconstruction1.View(view_id1)->Camera().GetPosition();
     positions2[i] =
         reconstruction2->View(view_id2)->Camera().GetPosition();
+  }
+
+  // Align the positions.
+  Eigen::Matrix3d rotation;
+  Eigen::Vector3d translation;
+  double scale;
+  AlignPointCloudsUmeyama(positions2,
+                          positions1,
+                          &rotation,
+                          &translation,
+                          &scale);
+
+  // Apply the similarity transformation to the reconstruction.
+  TransformReconstruction(rotation, translation, scale, reconstruction2);
+}
+
+void AlignReconstructionsRobust(
+    const double robust_error_threshold,
+    const Reconstruction& reconstruction1,
+    Reconstruction* reconstruction2) {
+  CHECK_NOTNULL(reconstruction2);
+
+  const std::vector<std::string> common_view_names =
+      FindCommonViewsByName(reconstruction1, *reconstruction2);
+
+  // Collect the positions of all common views.
+  std::vector<CameraCorrespondence> correspondences(common_view_names.size());
+  for (int i = 0; i < common_view_names.size(); i++) {
+    const ViewId view_id1 =
+        reconstruction1.ViewIdFromName(common_view_names[i]);
+    const ViewId view_id2 =
+        reconstruction2->ViewIdFromName(common_view_names[i]);
+    correspondences[i].camera1 =
+        reconstruction1.View(view_id1)->Camera().GetPosition();
+    correspondences[i].camera2 =
+        reconstruction2->View(view_id2)->Camera().GetPosition();
+  }
+
+  // Estimate with RANSAC.
+  RansacParameters params;
+  params.max_iterations = 1000;
+  params.use_mle = true;
+  params.error_thresh = robust_error_threshold * robust_error_threshold;
+  params.failure_probability = 1e-4;
+
+  CameraAlignmentEstimator estimator;
+  Ransac<CameraAlignmentEstimator> ransac(params, estimator);
+  CHECK(ransac.Initialize()) << "Could not initialize RANSAC for similarity "
+                                "transformation estimation.";
+  SimilarityTransformation sim_transform;
+  RansacSummary summary;
+  CHECK(ransac.Estimate(correspondences, &sim_transform, &summary))
+      << "Could not align models with RANSAC";
+  CHECK_GT(summary.inliers.size(), 0)
+      << "No inliers could be found for estimating the similarity "
+         "transformation. Try using a higher error threshold.";
+
+  // Align the reconstructions using the inliers.
+  std::vector<Eigen::Vector3d> positions1(summary.inliers.size());
+  std::vector<Eigen::Vector3d> positions2(summary.inliers.size());
+  for (int i = 0; i < summary.inliers.size(); i++) {
+    positions1[i] = correspondences[summary.inliers[i]].camera1;
+    positions2[i] = correspondences[summary.inliers[i]].camera2;
   }
 
   // Align the positions.
