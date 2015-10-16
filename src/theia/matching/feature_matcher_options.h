4e7eadb79ca3295064097ff280076a96893ba37d
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2015-10-16 11:06:31 -0700

    Matching is performed in memory by default
    Change-Id: I9501959d49aff0e92edae5c3004aed76cdce33bf

--- src/theia/matching/feature_matcher_options.h
+++ src/theia/matching/feature_matcher_options.h
@@ -49,20 +49,13 @@ struct FeatureMatcherOptions {
   // valid writeable directory.
   std::string keypoints_and_descriptors_output_dir = "";
 
-  // Matching may be performed in core (i.e. all in memory) or out-of-core. For
-  // the latter, features are written and read to/from disk as needed (utilizing
-  // an LRU cache). The out-of-core strategy is more scalable since the memory
-  // footprint is limited. Set this value to false to perform all-in-memory
-  // matching.
-  bool match_out_of_core = true;
-
   // We store the descriptors of up to cache_capacity images in the cache at a
   // given time. The higher the cache capacity, the more memory is required to
   // perform image-to-image matching.
   int cache_capacity = 128;
 
   // Only symmetric matches are kept.
-  bool keep_only_symmetric_matches = true;
+  bool keep_only_symmetric_matches = false;
 
   // Only keep the matches that pass the lowes ratio test such that the distance
   // of the best best match is less than lowes_ratio of the distance of the
