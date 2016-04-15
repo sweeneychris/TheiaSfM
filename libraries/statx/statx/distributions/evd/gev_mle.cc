496fa25c5329032b6fe1316b5d3e28432c3f0fec
Chris Sweeney <sweeney.chris.m@gmail.com>
2016-04-14 17:05:33 -0700

    Fix potential undefined M_PI in statx
    Change-Id: I3a6039ad83177ab95e5a92902dd783d0e6c73047

--- libraries/statx/statx/distributions/evd/gev_mle.cc
+++ libraries/statx/statx/distributions/evd/gev_mle.cc
@@ -37,6 +37,11 @@
 #include "statx/utils/common_funcs.h"
 #include <optimo/solvers/bfgs.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
+
+
 namespace statx {
 namespace distributions {
 namespace evd {
