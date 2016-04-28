85df122f82de99759a944c098c9b97d6240a828f
nosleduc <nosleduc@users.noreply.github.com>
2016-04-28 21:57:02 +0200

    Add weights to alignment
    Change-Id: I084a5fd78674d4dfc84bf39e15fc6ad213befd23

--- src/theia/sfm/transformation/align_point_clouds_test.cc
+++ src/theia/sfm/transformation/align_point_clouds_test.cc
@@ -38,6 +38,7 @@
 
 #include "gtest/gtest.h"
 #include "theia/math/util.h"
+#include "theia/util/random.h"
 #include "theia/sfm/transformation/align_point_clouds.h"
 
 namespace theia {
@@ -51,14 +52,10 @@ double kEpsilon = 1e-6;
 
 void UmeyamaSimpleTest() {
   std::vector<Vector3d> left = {
-      Vector3d(0.4, -3.105, 2.147),
-      Vector3d(1.293, 7.1982, -.068),
-      Vector3d(-5.34, 0.708, -3.69),
-      Vector3d(-.345, 1.987, 0.936),
-      Vector3d(0.93, 1.45, 1.079),
-      Vector3d(-3.15, -4.73, 2.49),
-      Vector3d(2.401, -2.03, -1.87),
-      Vector3d(3.192, -.573, 0.1),
+      Vector3d(0.4, -3.105, 2.147),  Vector3d(1.293, 7.1982, -.068),
+      Vector3d(-5.34, 0.708, -3.69), Vector3d(-.345, 1.987, 0.936),
+      Vector3d(0.93, 1.45, 1.079),   Vector3d(-3.15, -4.73, 2.49),
+      Vector3d(2.401, -2.03, -1.87), Vector3d(3.192, -.573, 0.1),
       Vector3d(-2.53, 3.07, -5.19)};
 
   const Matrix3d rotation_mat =
@@ -91,10 +88,110 @@ void UmeyamaSimpleTest() {
   ASSERT_LT(fabs(expected_scale - scale), kEpsilon);
 }
 
+// It is not easy to find a formula for the weights that always works
+// We need to make some statistics to see if the function works in most cases
+//
+// This test adds some noise on a given fraction of the points
+// We try to estimate the weights from the errors of the first pass (setting
+// weights to 1)
+// We can expect that the errors are where the noise was added
+// From this first result we can set a weight for each point.
+// The weight is given by 1.0/(1.0 + distance)
+// Where distance is the distance between right[i] and s * R * left[i] + T
+// estimated by a first pass
+// To see if the new estimation from these weights is better
+// we need to check that the new parameters are closer to the expected
+// parameters
+void UmeyamaWithWeigthsAndNoise() {
+  // Make some statistics
+  // 1000 is not a problem the algorith is fast
+  static const size_t kNumIterations = 1000;
+
+  // 15 % of noise
+  static const float kNoiseRatio = 0.15f;
+
+  // Percentage to consider the test succeeds (is it enough ?)
+  static const float kTestsSucceeded = 95.0f;
+
+  size_t succeeded = 0;
+  for (size_t iteration = 0; iteration < kNumIterations; ++iteration) {
+    // 4 pts required
+    const size_t num_points = static_cast<size_t>(theia::RandInt(4, 1000));
+    std::vector<Vector3d> left(num_points);
+    std::vector<double> weights(num_points, 1.0);
+
+    for (size_t i = 0; i < num_points; ++i) {
+      left[i] = Eigen::Vector3d::Random();
+    }
+
+    const Matrix3d rotation_mat =
+        Eigen::AngleAxisd(DegToRad(theia::RandDouble(0.0, 360.0)),
+                          Eigen::Vector3d::Random().normalized())
+            .toRotationMatrix();
+    const Vector3d translation_vec = Eigen::Vector3d::Random();
+    const double expected_scale = theia::RandDouble(0.001, 10);
+
+    // Transform the points.
+    std::vector<Vector3d> right;
+    for (size_t i = 0; i < left.size(); i++) {
+      const Vector3d transformed_point =
+          expected_scale * rotation_mat * left[i] + translation_vec;
+      right.emplace_back(transformed_point);
+    }
+
+    // Add noise on scale, point and translation
+    for (size_t i = 0, end = (size_t)(kNoiseRatio * num_points); i < end; ++i) {
+      const size_t k = static_cast<size_t>(theia::RandInt(0,  num_points));
+      const double noiseOnScale = expected_scale + theia::RandDouble(0, 10);
+      right[k] = noiseOnScale * rotation_mat * (left[k] + Vector3d::Random()) +
+                 translation_vec + Vector3d::Random();
+    }
+
+    // We need to find some weights
+    Matrix3d rotation_noisy;
+    Vector3d translation_noisy;
+    double scale_noisy;
+    AlignPointCloudsUmeyamaWithWeights(left, right, weights, &rotation_noisy,
+                                       &translation_noisy, &scale_noisy);
+    for (size_t i = 0; i < num_points; ++i) {
+      const double dist = (right[i] - (scale_noisy * rotation_noisy * left[i] +
+                                       translation_noisy))
+                              .norm();
+      weights[i] = 1.0 / (1.0 + dist);
+    }
+    //
+
+    Matrix3d rotation_weighted;
+    Vector3d translation_weighted;
+    double scale_weighted;
+    AlignPointCloudsUmeyamaWithWeights(left, right, weights, &rotation_weighted,
+                                       &translation_weighted, &scale_weighted);
+
+    // Check if the parameters are closer to real parameters ?
+    const bool condition_on_scale = (std::abs(scale_weighted - expected_scale) <
+                                     std::abs(scale_noisy - expected_scale));
+    const bool condition_on_translation =
+        (translation_vec - translation_weighted).norm() <
+        (translation_vec - translation_noisy).norm();
+    const bool condition_on_rotation =
+        (rotation_mat - rotation_weighted).norm() <
+        (rotation_mat - rotation_noisy).norm();
+
+    if (condition_on_scale && condition_on_translation && condition_on_rotation)
+      ++succeeded;
+  }
+
+  ASSERT_LE(kTestsSucceeded, 100.0 * succeeded / (0.0 + kNumIterations));
+}
+
 }  // namespace
 
 TEST(AlignPointCloudsUmeyama, SimpleTest) {
-  UmeyamaSimpleTest();
+    UmeyamaSimpleTest();
+}
+
+TEST(AlignPointCloudsUmeyamaWithWeights, WeightsAndNoise) {
+  UmeyamaWithWeigthsAndNoise();
 }
 
 }  // namespace theia
