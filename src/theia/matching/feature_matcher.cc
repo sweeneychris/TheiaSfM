867c13163387b7672b39acb5b5b7abe1d599bd95
Chris Sweeney <sweeney.chris.m@gmail.com>
2016-09-06 17:08:36 -0700

    Bug fix for matching from memory.
    Change-Id: I63e3a107f258bfef4920fed4b182e71bfab89ced

--- src/theia/matching/feature_matcher.cc
+++ src/theia/matching/feature_matcher.cc
@@ -63,19 +63,6 @@ namespace theia {
 
 FeatureMatcher::FeatureMatcher(const FeatureMatcherOptions& options)
     : options_(options) {
-  // Because the function that defines how the cache fetches features from disk
-  // is a member function, we need to bind it to this instance of FeatureMatcher
-  // and specify that it will take in 1 argument.
-  std::function<std::shared_ptr<KeypointsAndDescriptors>(const std::string&)>
-      fetch_features_from_cache =
-          std::bind(&FeatureMatcher::FetchKeypointsAndDescriptorsFromDisk, this,
-                    std::placeholders::_1);
-  // Initialize the LRU cache. NOTE: even though the Fetch method will be set up
-  // to retreive files from disk, it will only do so if
-  // options_.match_out_of_core is set to true.
-  keypoints_and_descriptors_cache_.reset(new KeypointAndDescriptorCache(
-      fetch_features_from_cache, options_.cache_capacity));
-
   if (options_.match_out_of_core) {
     CHECK_GT(options_.cache_capacity, 2)
         << "The cache capacity must be greater than 2 in order to perform out "
@@ -95,6 +82,21 @@ FeatureMatcher::FeatureMatcher(const FeatureMatcherOptions& options)
     // the maximum.
     options_.cache_capacity = std::numeric_limits<int>::max();
   }
+
+  // Because the function that defines how the cache fetches features from disk
+  // is a member function, we need to bind it to this instance of FeatureMatcher
+  // and specify that it will take in 1 argument.
+  std::function<std::shared_ptr<KeypointsAndDescriptors>(const std::string&)>
+      fetch_features_from_cache =
+          std::bind(&FeatureMatcher::FetchKeypointsAndDescriptorsFromDisk, this,
+                    std::placeholders::_1);
+
+  // Initialize the LRU cache. NOTE: even though the Fetch method will be set up
+  // to retreive files from disk, it will only do so if
+  // options_.match_out_of_core is set to true.
+  keypoints_and_descriptors_cache_.reset(new KeypointAndDescriptorCache(
+      fetch_features_from_cache, options_.cache_capacity));
+
 }
 
 void FeatureMatcher::AddImage(
