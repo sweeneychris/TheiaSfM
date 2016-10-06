43376995ccd7eefe5fdacd970dca04f4728cc121
Chris Sweeney <sweeney.chris.m@gmail.com>
2016-10-06 09:47:46 -0700

    Eigen serialization methods for arrays.
    Change-Id: I52f9b20d85553cb5357c8f9368aed0c4044f56da

--- src/theia/io/eigen_serializable.h
+++ src/theia/io/eigen_serializable.h
@@ -73,6 +73,36 @@ load(Archive& ar,
                  static_cast<std::size_t>(rows * cols * sizeof(_Scalar))));
 }
 
+template <class Archive, class _Scalar, int _Rows, int _Cols, int _Options,
+          int _MaxRows, int _MaxCols>
+inline typename std::enable_if<
+    traits::is_output_serializable<BinaryData<_Scalar>, Archive>::value,
+    void>::type
+save(Archive& ar, Eigen::Array<_Scalar, _Rows, _Cols, _Options, _MaxRows,
+                                _MaxCols> const& m) {
+  int32_t rows = m.rows();
+  int32_t cols = m.cols();
+  ar(rows);
+  ar(cols);
+  ar(binary_data(m.data(), rows * cols * sizeof(_Scalar)));
+}
+
+template <class Archive, class _Scalar, int _Rows, int _Cols, int _Options,
+          int _MaxRows, int _MaxCols>
+inline typename std::enable_if<
+    traits::is_input_serializable<BinaryData<_Scalar>, Archive>::value,
+    void>::type
+load(Archive& ar,
+     Eigen::Array<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols>& m) {
+  int32_t rows;
+  int32_t cols;
+  ar(rows);
+  ar(cols);
+  m.resize(rows, cols);
+  ar(binary_data(m.data(),
+                 static_cast<std::size_t>(rows * cols * sizeof(_Scalar))));
+}
+
 }  // namespace cereal
 
 #endif  // THEIA_IO_EIGEN_SERIALIZABLE_H_
