7a50eb1a2e4d370f77bf5838e9a8db57596ef08d
Chris Sweeney <sweeney.chris.m@gmail.com>
2017-02-20 00:23:16 -0800

    Missing include for new file
    Change-Id: I5709cf847627c7aa8e860649699a61c31627db20

--- include/theia/theia.h
+++ include/theia/theia.h
@@ -48,6 +48,7 @@
 #include "theia/image/keypoint_detector/sift_parameters.h"
 #include "theia/io/eigen_serializable.h"
 #include "theia/io/import_nvm_file.h"
+#include "theia/io/populate_image_sizes.h"
 #include "theia/io/read_1dsfm.h"
 #include "theia/io/read_bundler_files.h"
 #include "theia/io/read_calibration.h"
