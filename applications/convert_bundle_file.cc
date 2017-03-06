6f3921f1bc192b2e8a3689f5df254337cdc11b22
Chris Sweeney <sweeney.chris.m@gmail.com>
2017-03-06 14:23:25 -0800

    Option to extract image sizes and principal point when loading bundler files.
    Change-Id: I022923b42ce4cd3362aec02fed0e27483f7d47df

--- applications/convert_bundle_file.cc
+++ applications/convert_bundle_file.cc
@@ -44,7 +44,10 @@ DEFINE_string(lists_file, "", "Input bundle lists file.");
 DEFINE_string(bundle_file, "", "Input bundle file.");
 DEFINE_string(output_reconstruction_file, "",
               "Output reconstruction file in binary format.");
-
+DEFINE_string(images_directory, "",
+              "Directory of input images. This is used to extract the "
+              "principal point and image dimensions since Bundler does not "
+              "provide those.");
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
   THEIA_GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
@@ -55,6 +58,14 @@ int main(int argc, char* argv[]) {
                                 FLAGS_bundle_file,
                                 &reconstruction))
       << "Could not read Bundler files.";
+  if (FLAGS_images_directory.size() > 0) {
+    CHECK(theia::PopulateImageSizesAndPrincipalPoints(FLAGS_images_directory,
+                                                      &reconstruction));
+  } else {
+    LOG(INFO) << "The image directory was not provided so the principal point "
+                 "and image dimensions are assumed to be zero. Proceed with "
+                 "caution!";
+  }
 
   CHECK(WriteReconstruction(reconstruction, FLAGS_output_reconstruction_file))
       << "Could not write out reconstruction file.";
