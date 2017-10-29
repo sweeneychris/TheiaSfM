81a0ac2c04422c58661f806b9a454acc0f674732
Aleksander Holynski <holynski@cs.washington.edu>
2017-10-29 15:45:00 -0700

    Adding COLMAP exporter
    Change-Id: I6fd5b7df90515c7b1d908a841e3d0d6c3d87403b

---
+++ applications/export_colmap_files.cc
@@ -0,0 +1,61 @@
+// Copyright (C) 2014 The Regents of the University of California (Regents).
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of The Regents or University of California nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Aleksander Holynski (holynski@cs.washington.edu)
+
+#include <Eigen/Core>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <theia/io/write_colmap_files.h>
+#include <theia/theia.h>
+
+#include <algorithm>
+#include <string>
+
+DEFINE_string(output_folder, "", "Folder to output the colmap files.");
+DEFINE_string(input_reconstruction_file,
+              "",
+              "Input Theia reconstruction (.bin).");
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  THEIA_GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Load the reconstuction.
+  theia::Reconstruction reconstruction;
+  CHECK(theia::ReadReconstruction(FLAGS_input_reconstruction_file,
+                                  &reconstruction))
+      << "Could not read reconstruction.";
+
+  CHECK(WriteColmapFiles(reconstruction, FLAGS_output_folder))
+      << "Could not write out reconstruction file.";
+  return 0;
+}
