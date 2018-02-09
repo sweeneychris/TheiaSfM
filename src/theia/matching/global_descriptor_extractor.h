0797b5978032b3337c04154e4bcc13a89471d97a
Benjamin <bbsmith1@mix.wvu.edu>
2018-02-09 14:44:19 -0500

    Add global descriptor extractor header.
    Change-Id: I717388bca8e36297cbdc82f95b5c422c7172ccdf

---
+++ src/theia/matching/global_descriptor_extractor.h
@@ -0,0 +1,103 @@
+// Copyright (C) 2018 The Regents of the University of California (Regents).
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of The Regents or University of California nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Authors: Victor Fragoso (victor.fragoso@mail.wvu.edu)
+//        : Benjamin Smith (bbsmith1@mix.wvu.edu)
+
+#ifndef THEIA_MATCHING_GLOBAL_DESCRIPTOR_EXTRACTOR_H_
+#define THEIA_MATCHING_GLOBAL_DESCRIPTOR_EXTRACTOR_H_
+
+#include <vector>
+#include <glog/logging.h>
+#include <Eigen/Core>
+
+namespace theia {
+// Parameters for the global descriptor extractor.
+struct GlobalDescriptorExtractorParameters {
+  // Number of centroids. This parameter is used for quantization in
+  // VLAD via k-means and FisherVector via the modes in the GMM model.
+  int num_centroids;
+  // Number of dimensions in local descriptors.
+  int num_dimensions;
+  // TODO(vfragoso): Add extra parameters for other descriptor types.
+};
+
+// This class computes a global descriptor for a given image. The class is
+// templated to allow a wide range of local descriptors, global descriptor
+// types, and training descriptor types.
+//
+// The use of an extractor should follow the next procedure:
+//
+// 1. After creation, initialize the instance by calling Initialize().
+// 2. Train the extractor by calling Train().
+// 3. Compute the global descriptor by calling Extract().
+// TODO(vfragoso): Declare a dummy training descriptor type since hashed
+// descriptor does not really need a training descriptor type.
+class GlobalDescriptorExtractor {
+ public:
+  GlobalDescriptorExtractor(const GlobalDescriptorExtractorParameters& params) :
+      params_(params) {}
+  virtual ~GlobalDescriptorExtractor() {}
+
+  // Initializes the global descriptor extractor.
+  virtual bool Initialize() {
+    return true;
+  }
+  
+  // Trains the instance. Returns true upon success and false otherwise.
+  // Params:
+  //   training_descriptors  The training local descriptors by using specific
+  //     a TrainingDescriptorType (e.g., vector of Eigens (matrix)).
+  bool Train(const std::vector<Eigen::VectorXf>& training_descriptors) {
+    LOG(FATAL) << "Invalid call to the descriptor extractor";
+    return false;
+  }
+
+  // Computes the global descriptor from information about the image to
+  // describe. The method returns true upon success and false otherwise.
+  // Params:
+  //   input_information  The input image information.
+  //   global_descriptor  The computed global descriptor.
+  bool Extract(const std::vector<Eigen::VectorXf> input_information,
+               Eigen::VectorXf* global_descriptor) const {
+    LOG(FATAL) << "Invalid call to the descriptor extractor";
+    return false;
+  }
+
+ protected:
+  // Parameters for global descriptors.
+  const GlobalDescriptorExtractorParameters& params_;
+};
+
+}  // namespace theia
+
+#endif  // THEIA_MATCHING_GLOBAL_DESCRIPTOR_EXTRACTOR_H_
