a8e45a1520b2cd60efed465d3c8bf5f69c7830b7
Victor Fragoso <geektor2007@gmail.com>
2018-12-09 11:09:51 -0800

    Adding initial files for UPNP implementations.
    Change-Id: Ic5ee44ca8c9564028244d4fe3aef9b7a7ddea73e

---
+++ src/theia/sfm/pose/upnp.cc
@@ -0,0 +1,53 @@
+// Copyright (C) 2018 The Regents of the University of California (Regents).
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of The Regents or University of California nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Victor Fragoso (victor.fragoso@mail.wvu.edu)
+
+#include "theia/sfm/pose/upnp.h"
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <vector>
+
+namespace theia {
+
+// TODO(vfragoso): Document me!
+void Upnp(const std::vector<Eigen::Vector3d>& ray_origins,
+          const std::vector<Eigen::Vector3d>& ray_directions,
+          const std::vector<Eigen::Vector3d>& world_points,
+          std::vector<Eigen::Quaterniond>* solution_rotations,
+          std::vector<Eigen::Vector3d>* solution_translations) {
+  
+}
+
+}  // namespace theia
+
